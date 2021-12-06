e648fe40578811375e37563f5bb2361c71bbe7e3
Karol Sitarz <ka.sitarz@gmail.com>
2021-12-06 03:34:24 +0100

    add conditional Auth0 for cypress tests

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
+import Auth0Provider from 'next-auth/providers/auth0'
 import { PrismaExtendedAdapter } from 'shared/utils/PrismaExtendedAdapter'
 
 export default NextAuth({
@@ -8,9 +9,18 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
+    ...(!process.env.CYPRESS_TEST
+      ? []
+      : [
+          Auth0Provider({
+            clientId: process.env.AUTH0_CLIENT_ID,
+            clientSecret: process.env.AUTH0_CLIENT_SECRET,
+            issuer: process.env.AUTH0_ISSUER,
+          }),
+        ]),
   ],
   pages: {
-    signIn: '../../',
+    // signIn: '../../',
     error: '../../error',
     signOut: '../../',
   },
